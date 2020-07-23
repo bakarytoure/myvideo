0fe8bf4102ee5e5e49fa1a888b504e7772234e49
bakarytoure <bakarytoure57@gmail.com>
2020-07-23 09:21:36 +0000

    Updates

---
+++ precache-manifest.1f59df043dacfd3c9e5ef093b8fadd35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a43115a4c72ad49854006820ab6b02aa",
+    "url": "/myvideo/index.html"
+  },
+  {
+    "revision": "843b5b70a75de45469ce",
+    "url": "/myvideo/static/css/main.50afe346.chunk.css"
+  },
+  {
+    "revision": "28ee0f524736da7b286f",
+    "url": "/myvideo/static/js/2.c2322cf7.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/myvideo/static/js/2.c2322cf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "843b5b70a75de45469ce",
+    "url": "/myvideo/static/js/main.e236f7bb.chunk.js"
+  },
+  {
+    "revision": "a911cd57331842560975",
+    "url": "/myvideo/static/js/runtime-main.47977f6c.js"
+  },
+  {
+    "revision": "097cdad55ce419e83705f45fe2b0239f",
+    "url": "/myvideo/static/media/Myproject-1.097cdad5.m4v"
+  }
+]);
